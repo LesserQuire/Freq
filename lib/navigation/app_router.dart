a0f4264d0d3917ab27cd27c3d08a8fef3e07fef9
karthiktamiledu <karthiktamiledu@gmail.com>
2025-11-04 20:37:48 -0800

    fixed route transitions

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -15,9 +15,9 @@ import '../pages/splash_page.dart';
 final router = GoRouter(
   initialLocation: '/splash',
   routes: [
-    GoRoute(path: '/splash', pageBuilder: (c, s) => _crossfadePage(const SplashPage(), s)),
-    GoRoute(path: '/signup', pageBuilder: (c, s) => _crossfadePage(const SignupPage(), s)),
-    GoRoute(path: '/player', pageBuilder: (c, s) => _crossfadePage(const PlayerPage(), s)),
+    GoRoute(path: '/splash', pageBuilder: (c, s) => _fadePage(const SplashPage(), s)),
+    GoRoute(path: '/signup', pageBuilder: (c, s) => _fadePage(const SignupPage(), s)),
+    GoRoute(path: '/player', pageBuilder: (c, s) => _fadePage(const PlayerPage(), s)),
     ShellRoute(
       builder: (context, state, child) => Scaffold(
         body: child,
@@ -26,46 +26,35 @@ final router = GoRouter(
       routes: [
         GoRoute(
           path: '/home',
-          pageBuilder: (c, s) =>
-              _crossfadePage(HomePage(isPremium: s.uri.queryParameters['premium'] == 'true'), s),
+          pageBuilder: (c, s) => _fadePage(
+              HomePage(isPremium: s.uri.queryParameters['premium'] == 'true'), s),
         ),
-        GoRoute(path: '/account', pageBuilder: (c, s) => _crossfadePage(const AccountPage(), s)),
-        GoRoute(path: '/search', pageBuilder: (c, s) => _crossfadePage(const SearchPage(), s)),
-        GoRoute(path: '/add-url', pageBuilder: (c, s) => _crossfadePage(const AddUrlPage(), s)),
-        GoRoute(path: '/search-details', pageBuilder: (c, s) => _crossfadePage(const SearchDetailsPage(), s)),
+        GoRoute(path: '/account', pageBuilder: (c, s) => _fadePage(const AccountPage(), s)),
+        GoRoute(path: '/search', pageBuilder: (c, s) => _fadePage(const SearchPage(), s)),
+        GoRoute(path: '/add-url', pageBuilder: (c, s) => _fadePage(const AddUrlPage(), s)),
+        GoRoute(path: '/search-details', pageBuilder: (c, s) => _fadePage(const SearchDetailsPage(), s)),
         GoRoute(
           path: '/details',
           pageBuilder: (c, s) {
             final extra = s.extra is Map<String, dynamic> ? s.extra as Map<String, dynamic> : null;
-            return _crossfadePage(DetailsPage(extra: extra), s);
+            return _fadePage(DetailsPage(extra: extra), s);
           },
         ),
-        GoRoute(path: '/chat', pageBuilder: (c, s) => _crossfadePage(const ChatPage(), s)),
+        GoRoute(path: '/chat', pageBuilder: (c, s) => _fadePage(const ChatPage(), s)),
       ],
     ),
   ],
 );
 
-CustomTransitionPage _crossfadePage(Widget child, GoRouterState state) {
+CustomTransitionPage _fadePage(Widget child, GoRouterState state) {
   return CustomTransitionPage(
-    key: state.pageKey,
     child: child,
+    transitionDuration: const Duration(milliseconds: 300),
+    reverseTransitionDuration: const Duration(milliseconds: 300),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      final fadeIn = FadeTransition(
+      return FadeTransition(
         opacity: animation,
         child: child,
-      );
-
-      final fadeOut = FadeTransition(
-        opacity: Tween<double>(begin: 1.0, end: 0.0).animate(secondaryAnimation),
-        child: child,
-      );
-
-      return Stack(
-        children: [
-          fadeOut,
-          fadeIn,
-        ],
       );
     },
   );
